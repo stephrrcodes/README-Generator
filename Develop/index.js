a2335b2953289b28c6a065ab74590bad7edb4cb5
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-04-24 19:09:06 -0700

    fixed generateMarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require("fs");
-const utils = require("utils");
+const utils = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [];
 inquirer
