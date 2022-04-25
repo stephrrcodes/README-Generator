b85b7071c293452cee69c9f5354cfe0c6251f242
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-04-24 18:57:32 -0700

    created array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,42 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require("fs");
+const utils = require("utils");
 // TODO: Create an array of questions for user input
 const questions = [];
-
+inquirer
+  .prompt(
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?' 
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of the project?' 
+    },
+    {
+        type: 'input',
+        name: 'installation instructions',
+        message: 'What are the installation instructions of the project?' 
+    },
+    {
+        type: 'input',
+        name: 'usage information',
+        message: 'What is the usage information of the project?' 
+    },
+    {
+        type: 'input',
+        name: 'contribution guidelines',
+        message: 'Who has contributed to the project?' 
+    },
+    {
+        type: 'input',
+        name: 'test instructions',
+        message: 'What tests are there of the project?' 
+    },
+    )
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
